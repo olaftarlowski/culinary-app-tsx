0a4081630fbef8d953c8369016abf30cc0173c6e
OlfiDell <bobr.rozrabiaka123@gmail.com>
2022-08-03 17:10:35 +0200

    added sample response from Airtable

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,34 @@ import { default as api, Records } from "./services/api";
 import { DataList } from "./components/";
 import "./App.css";
 
+// const SAMPLE_RESPONSE = {
+//   "records": [
+//     {
+//       "id": "recmuG4HckO4ox35Y",
+//       "createdTime": "2022-07-31T19:45:35.000Z",
+//       "fields": {
+//         "TAKIJEDEN": [
+//           "recmuG4HckO4ox35Y",
+//           "rec3g2pR0GIWnl5OF",
+//           "rec6kVf4m6y8XGVDb"
+//         ],
+//         "selekty": [
+//           "select 1"
+//         ],
+//         "Name": "1st wpis",
+//         "Notes": "description",
+//         "Status": "2022-07-31T19:45:35.000Z",
+//         "qwerty": [
+//           "1st wpis",
+//           "dsfgsdfg",
+//           "dfgsdf"
+//         ]
+//       }
+//     }
+//   ],
+//   "offset": "itrWgkmdaIpR9s3cH/rec3g2pR0GIWnl5OF"
+// }
+
 
 const App = () => {
   const [airtableData, setAirtableData] = useState<Array<Records>>([]);
